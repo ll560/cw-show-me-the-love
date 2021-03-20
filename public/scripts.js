045878138379ffdd7780b825b5a5274dd3088515
ll560 <llavia6@gmail.com>
2021-03-20 12:50:55 -0400

    first commit

---
+++ public/scripts.js
@@ -0,0 +1,21 @@
+
+
+
+
+function evaluateLove(){
+    const loveName =prompt('Whats is your name?')
+    const color = prompt(`${loveName} is a good name. What is your favorite color?`)
+    switch (color) {
+        case 'green':
+        case 'red':
+        case 'blue':
+            alert ('I think we are a match!');
+            break;
+        default:
+            alert('Thank you next!');
+            break;
+    }
+
+
+   
+}
